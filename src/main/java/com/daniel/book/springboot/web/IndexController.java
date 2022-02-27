f35ba0f74f18cd1a6eddb0b318a56f0ee415273a
DANIEL <yuntaehui77@gmail.com>
2022-02-27 22:51:09 +0900

    5장 - 스프링부트 1.5와 2.0에서 시큐리티 설정 차이점 - 스프링 시큐리티를 이용한 구글 로그인 연동방법

--- src/main/java/com/daniel/book/springboot/web/IndexController.java
+++ src/main/java/com/daniel/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.daniel.book.springboot.web;
 
+import com.daniel.book.springboot.config.auth.dto.SessionUser;
 import com.daniel.book.springboot.service.PostsService;
 import com.daniel.book.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +9,22 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
     public String index(Model model) {
         model.addAttribute("posts", postsService.findAllDesc());
+        SessionUser user = (SessionUser) httpSession.getAttribute("user"); // CustomOAuth2UserService에서 로그인 성공시 세션에 SessionUser를 저장하도록 구성했습니다.
+        if (user != null) { // 세션에 값이 있을때만 model에 userName으로 등록합니다.
+            model.addAttribute("userName", user.getName());
+        }
         return "index";
     }
 
