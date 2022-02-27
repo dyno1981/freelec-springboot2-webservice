a5fc25c37f24a24b599f410c8ecfa1c22c1af52e
DANIEL <yuntaehui77@gmail.com>
2022-02-27 21:32:18 +0900

    4장 - 서버템플릿엔진과 클라이언트 템플릿엔진의 차이 - 머스테치의 기본 사용방법 - 스프링부트에서의 화면 처리 방식 - js/css 선언 위치를 다르게 하여 웹사이트의 로딩 속도 향상하는 방법 - js 객체를 이용하여 브라우저의 전역 변수 충돌 문제를 회피하는 방법

--- src/main/java/com/daniel/book/springboot/service/PostsService.java
+++ src/main/java/com/daniel/book/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.daniel.book.springboot.service;
 
 import com.daniel.book.springboot.domain.posts.Posts;
 import com.daniel.book.springboot.domain.posts.PostsRepository;
+import com.daniel.book.springboot.web.dto.PostsListResponseDto;
 import com.daniel.book.springboot.web.dto.PostsResponseDto;
 import com.daniel.book.springboot.web.dto.PostsSaveRequestDto;
 import com.daniel.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -30,5 +34,18 @@ public class PostsService {
     public PostsResponseDto findById(Long id) {
         Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findallDesc().stream()
+                .map(PostsListResponseDto::new) // .map(posts -> new PostsListResponseDto(posts)) 와 같다
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        postsRepository.delete(posts);
     }
 }
