c4a9fe08fe0c98dc896b154a3559f4b0d1fb540e
DANIEL <yuntaehui77@gmail.com>
2022-02-27 20:15:37 +0900

    3장 - JPA/Hibernate/Spring Data Jpa 관계 - Spring Data Jpa 관계형 데이터베이스를 재체지향적으로 관리하는 법 - JPA 더티체킹을 이용 Update 쿼리 없이 테이블 수정 가능.. (심화 공부 필요) - JPA Auditing을 이요하여 등록/수정 시간을 자동화 하는 법

---
+++ src/main/java/com/daniel/book/springboot/service/PostsService.java
@@ -0,0 +1,34 @@
+package com.daniel.book.springboot.service;
+
+import com.daniel.book.springboot.domain.posts.Posts;
+import com.daniel.book.springboot.domain.posts.PostsRepository;
+import com.daniel.book.springboot.web.dto.PostsResponseDto;
+import com.daniel.book.springboot.web.dto.PostsSaveRequestDto;
+import com.daniel.book.springboot.web.dto.PostsUpdateRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    public final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id) {
+        Posts entity = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = " + id));
+        return new PostsResponseDto(entity);
+    }
+}
---
+++ src/test/java/com/daniel/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,63 @@
+package com.daniel.book.springboot.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After // JUnit에서 단위 테스트가 끝날때마다 수행되는 메소드를 지정, 여러 테스트가 동시에 수행되면 테스트용 데이터베이스인 H2에 데이터가 남아 테스트가 실패할 수 있습니다.
+    public void cleanup() {
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        // given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder() // 테이블 posts에 insert/update 쿼리를 실행합니다. id값이 있다면 update, 없다면 insert쿼리가 실행됩니다.
+                .title(title)
+                .content(content)
+                .author("daniel@daniel.com")
+                .build());
+        // when
+        List<Posts> postsList = postsRepository.findAll(); // posts 테이블에 있는 모든 데이터를 조회해오는 메소드
+        // then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+    @Test
+    public void BaseTimeEntity_등록() {
+        // given
+        LocalDateTime now = LocalDateTime.of(2019, 6, 4, 0, 0, 0);
+        postsRepository.save(Posts.builder()
+                .title("title")
+                .content("content")
+                .author("author")
+                .build());
+        // when
+        List<Posts> postsList = postsRepository.findAll();
+        // then
+        Posts posts = postsList.get(0);
+        System.out.println(">>>>>>> greatedDate = " + posts.getCreatedDate() + ", modifiedDate = " + posts.getModifiedDate());
+        assertThat(posts.getCreatedDate()).isAfter(now);
+        assertThat(posts.getModifiedDate()).isAfter(now);
+    }
+}
